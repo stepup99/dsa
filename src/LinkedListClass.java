19453a6c5f6b7c9aacfd96e54463ddf1cfc56040
Anuj Gupta <anuj_gupta3@apple.com>
2022-12-30 00:41:12 +0530

    lisnked list new changes commit

--- src/LinkedListClass.java
+++ src/LinkedListClass.java
@@ -6,6 +6,34 @@ public class LinkedListClass {
         list.add("Apple");
         list.add("Mango");
         list.add("veg");
-        System.out.println(list);
+//        System.out.println(list);
+
+
+        LinkedList<Integer> integerList[] = new LinkedList[3];
+        integerList[0] = new LinkedList<>();
+        integerList[0].add(1);
+        integerList[0].add(2);
+        integerList[1] = new LinkedList<>();
+        integerList[1].add(2);
+        integerList[2] = new LinkedList<>();
+        integerList[2].add(3);
+
+
+        System.out.println(integerList[0]);
+
+        for(LinkedList n: integerList){
+            System.out.println(n);
+        }
+
+        Boolean[] flags = new Boolean[2];
+        flags[0] = true;
+        flags[1] = false;
+
+
+        for(Boolean i:flags){
+            System.out.println(i);
+        }
+
+
     }
 }
