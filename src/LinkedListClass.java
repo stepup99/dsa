84100e6eccb5fce6a5156f4198ebd7214b0bc4d5
Anuj Gupta <anuj_gupta3@apple.com>
2022-12-29 23:48:56 +0530

    adjacency list

---
+++ src/LinkedListClass.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+
+public class LinkedListClass {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+        list.add("Apple");
+        list.add("Mango");
+        list.add("veg");
+        System.out.println(list);
+    }
+}
